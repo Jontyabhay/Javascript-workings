fd2954f95d77c26e549d7d30822d3b1e6b3d43f6
Akshay Mishra <42304248+Jontyabhay@users.noreply.github.com>
2020-05-10 23:18:38 +0530

    kr do

--- SchoolCatalogue.js
+++ SchoolCatalogue.js
@@ -67,7 +67,7 @@ class HighSchool extends School
 
 const lorraineHansbury = new PrimarySchool('Lorraine Hansbury',514,'Students must be picked up by a parent, guardian, or a family member over the age of 13.');
 lorraineHansbury.quickFacts();
-School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']);
+console.log(School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']));
 
 const alSmith = new HighSchool('Al E. Smith',415,['Baseball', 'Basketball', 'Volleyball', 'Track and Field']);
 alSmith.sportsTeams;
