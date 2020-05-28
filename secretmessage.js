23f8bd968f5b0c0ecca518479428913162f551db
Akshay Mishra <42304248+Jontyabhay@users.noreply.github.com>
2020-05-29 01:40:58 +0530

    Create secretmessage.js

---
+++ secretmessage.js
@@ -0,0 +1,12 @@
+let secretMessage = 
+['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 
+'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+secretMessage.pop();
+secretMessage.length;
+secretMessage.push('to');
+secretMessage.push('Program');
+secretMessage[7] = 'right';
+secretMessage.shift();
+secretMessage.unshift('Programming');
+secretMessage.splice(6,5,'know');
+console.log(secretMessage.join(' '));
